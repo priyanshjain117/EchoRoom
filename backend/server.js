79fa37cb134d5add61a8bdd3429f8497347e8949
priyanshjain117 <jainpriyanshu117@gmail.com>
2025-06-16 14:04:41 +0530

    firebase connected to backend

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,24 @@ const express = require('express');
 const admin = require("firebase-admin");
 const cors = require('cors');
 
-const app=express();
+require('dotenv').config();
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+
+admin.initializeApp({
+    credential: admin.credential.cert({
+        projectId: process.env.PROJECT_ID,
+        clientEmail: process.env.CLIENT_EMAIL,
+        privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    })
+});
+
+const db=admin.firestore();
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
