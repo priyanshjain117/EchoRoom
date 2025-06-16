04f62b8e2e76d7641b01b9275513e63f09da975f
priyanshjain117 <jainpriyanshu117@gmail.com>
2025-06-16 17:43:03 +0530

    error fixed: working on it

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.PROJECT_ID,
         clientEmail: process.env.CLIENT_EMAIL,
-        privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        privateKey: process.env.PRIVATE_KEY,
     })
 });
 
