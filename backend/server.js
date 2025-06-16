47252e1225e6c688b81875a75cfae7cb1d12949c
priyanshjain117 <jainpriyanshu117@gmail.com>
2025-06-16 14:41:26 +0530

    firebase notification sending setuped

--- backend/server.js
+++ backend/server.js
@@ -18,29 +18,43 @@ admin.initializeApp({
     })
 });
 
-const db=admin.firestore();
+const db = admin.firestore();
 
-app.post('/send-room-notification',async (req,res)=>{
+app.post('/send-room-notification', async (req, res) => {
     const { roomId, senderUid, title, body } = req.body;
-    const roomDoc=await db.collection('rooms').doc(roomId).get();
-    const roomData=roomDoc.data();
-    if (!roomData || !roomData.members) return res.status(404).send("Room not found");
+    try {
+        const roomDoc = await db.collection('rooms').doc(roomId).get();
+        const roomData = roomDoc.data();
+        if (!roomData || !roomData.members) return res.status(404).send("Room not found");
 
-    const members = roomData.members.filter(uid => uid !== senderUid);
-    const tokens = [];
+        const members = roomData.members.filter(uid => uid !== senderUid);
+        const tokens = [];
 
-    for(const uid in members){
-        const uid = await db.collection('users').doc(uid).get();
-        if (userDoc.exists && userDoc.data().fcmToken){
-            tokens.push(userDoc.data().fcmToken);
+        for (const uid in members) {
+            const uid = await db.collection('users').doc(uid).get();
+            if (userDoc.exists && userDoc.data().fcmToken) {
+                tokens.push(userDoc.data().fcmToken);
+            }
         }
-    }
 
-    if (tokens.length === 0) return res.status(200).send("No tokens to send");
+        if (tokens.length === 0) return res.status(200).send("No tokens to send");
+
+        const message = {
+            'notification': { title, body },
+            tokens,
+        }
+
+        const response = await admin.messaging().sendMulticast(message);
+        res.status(200).send(`✅ Sent to ${response.successCount} users`);
+
+    } catch (error) {
+        console.error("❌ Error sending notification:", err);
+        res.status(500).send("Server error");
+    }
 
 
 });
 
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
